cfe76331fc3c28a848de5343d6a1aac488c00387
Abdoulaye Latyr SENE <latyr0503@gmail.com>
2025-01-07 11:03:27 +0000

    cors correction v2

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Utilisez la méthode intégrée pour activer CORS
   app.enableCors({
-    origin: 'http://localhost:5173', // Autoriser toutes les origines (pour le développement local)
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
   });
-
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
