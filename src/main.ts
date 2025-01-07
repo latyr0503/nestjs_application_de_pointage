cba085eb14c36c438568f927b8c3e9a3b4d0ec59
Abdoulaye Latyr SENE <latyr0503@gmail.com>
2025-01-07 11:54:23 +0000

    correctionnn v2

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,21 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: [
+      '*',
+      'http://localhost:5173',
+      'https://react-application-de-gestion-de-budget.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
-  await app.listen(process.env.PORT || 3000);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    console.log(`Application is running on: http://localhost:${port}`);
+  });
 }
+
 bootstrap();
