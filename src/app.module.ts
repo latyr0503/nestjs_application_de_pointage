135baa3769cce5934e91392923ba7f254adb59f7
Abdoulaye Latyr SENE <latyr0503@gmail.com>
2025-01-07 09:43:06 +0000

    configuration .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { RevenuModule } from './revenu/revenu.module';
 import { DepenseModule } from './depense/depense.module';
-
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+dotenv.config();
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'bd_gestion_budget',
-      autoLoadEntities: true,
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
+      logging: true,
+      ssl: {
+        ca: fs.readFileSync('./certs/ca-certificate.crt').toString(),
+      },
+      autoLoadEntities: true,
     }),
     RevenuModule,
     DepenseModule,
