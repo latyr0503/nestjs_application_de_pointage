33c77d9fd6c7ecf101c1b56d550ebf0f2b163e9a
Abdoulaye Latyr SENE <latyr0503@gmail.com>
2025-05-24 19:17:11 +0000

    doc

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001'],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
